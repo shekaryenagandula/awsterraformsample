881fa5e180f1deb93b6f22c4fffed5c62b17bcce
azurelabpractice <azurelabpractice2111@gmail.com>
2023-11-08 14:33:44 +0530

    initial commit

--- backends.tf
+++ backends.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "http" {
-    address="https://gitlab.com/api/v4/projects/51982406/terraform/state/default"
-    lock_address="https://gitlab.com/api/v4/projects/51982406/terraform/state/default/lock"
-    unlock_address="https://gitlab.com/api/v4/projects/51982406/terraform/state/default/lock"
-  }  
+    address="https://gitlab.com/api/v4/projects/51983496/terraform/state/default"
+    lock_address="https://gitlab.com/api/v4/projects/51983496/terraform/state/default/lock"
+    unlock_address="https://gitlab.com/api/v4/projects/51983496/terraform/state/default/lock"
+}
 }
 
