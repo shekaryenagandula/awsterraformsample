5481eec7a53269104f846965746b49e63a7026be
azurelabpractice <azurelabpractice2111@gmail.com>
2023-11-08 19:01:32 +0530

    initial commit

--- modules/Compute/EC2/main.tf
+++ modules/Compute/EC2/main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "web" {
   subnet_id = var.subnet_id
 
   tags = {
-    Name=format(each.key,index( keys(var.server_names), each.value))
+    Name=format(each.key,index( var.server_names, each.value))
     Terraform   = "true"
     Environment = "dev"
   }
