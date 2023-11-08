a53f635f7aeeab9fe94e6611440c4d83e26534b3
azurelabpractice <azurelabpractice2111@gmail.com>
2023-11-08 14:39:43 +0530

    initial commit

--- backends.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "http" {
-    address="https://gitlab.com/api/v4/projects/51983496/terraform/state/default"
-    lock_address="https://gitlab.com/api/v4/projects/51983496/terraform/state/default/lock"
-    unlock_address="https://gitlab.com/api/v4/projects/51983496/terraform/state/default/lock"
-}
-}
-
---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.24.0"
+    }
+  }
+  backend "http" {
+  # address="https://gitlab.com/api/v4/projects/51983496/terraform/state/default"
+  # lock_address="https://gitlab.com/api/v4/projects/51983496/terraform/state/default/lock"
+  # unlock_address="https://gitlab.com/api/v4/projects/51983496/terraform/state/default/lock"
+  }
+}
+
+provider "aws" {
+  region="us-east-1"
+}
