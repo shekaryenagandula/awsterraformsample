fc6c8de964de7f6e18aa63a4df696a8d6a4f1371
azurelabpractice <azurelabpractice2111@gmail.com>
2023-11-08 16:30:29 +0530

    initial commit

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-output "vpc_id" {
-  description = "vpc ID and name"
-  value = try(module.vpc_eus_project) 
-}
-output "subnet_data" {
-  description = "subnet details"
-  value = try(module.aws_subnet)
-}
-output "ec2_details" {
-  description = "ec2 data"
-  value = try(module.ec2_eus_project)
-}
+# output "vpc_id" {
+#   description = "vpc ID and name"
+#   value = try(module.vpc_eus_project) 
+# }
+# output "subnet_data" {
+#   description = "subnet details"
+#   value = try(module.aws_subnet)
+# }
+# output "ec2_details" {
+#   description = "ec2 data"
+#   value = try(module.ec2_eus_project)
+# }
